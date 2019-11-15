33965b85bc7909a27b7a6422a1c3a9c758b3e5ff
Andrew Ackerman <andrew.ackers@gmail.com>
2019-11-15 11:17:36 -0800

    Submitting MVP before styling

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { Container, Row } from "reactstrap";
+import CharacterCard from './CharacterCard';
 
 export default function SearchForm() {
- 
+  const [character, setCharacter] = useState([]);
+  const [query, setQuery] = useState("");
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        const characters = response.data.results.filter(character =>
+          character.name.toLowerCase().includes(query.toLowerCase())
+        );
+        console.log("Rick and Morty characters", response);
+        setCharacter(characters);
+      });
+  }, [query]);
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <div className="characters">
+      <form className="search">
+        <input
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          name="name"
+          tabIndex="0"
+          className="prompt search-name"
+          placeholder="search by name"
+          autoComplete="off"
+        />
+      </form>
+      <div className="character">
+        <Container>
+          <Row>
+            {character.map(card => {
+              return (
+                <CharacterCard
+                  key={card.created}
+                  name={card.name}
+                  gender={card.gender}
+                  species={card.species}
+                  origin={card.origin}
+                  status={card.status}
+                />
+              )
+            })}
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 }
